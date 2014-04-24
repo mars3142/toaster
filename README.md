4d1b3f0bac5e5e32088236a0e777d7fb2de75d9b
petsie <p.siegmund@hanseatics.com>
2014-04-24 13:26:10 +0200

    * added copyright info

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-toaster
+Toaster
 =======
 
-Toastlogger for Android
+Records every toast message including the sender package. May be helpful if you get messages and don't know, which app sends it.
+It has to be activated as accessibility service.
--- app/src/main/java/org/mars3142/android/toaster/comparator/ToastCardComparator.java
+++ app/src/main/java/org/mars3142/android/toaster/comparator/ToastCardComparator.java
@@ -1,3 +1,22 @@
+/*
+Copyright 2014 Peter Siegmund
+
+This file is part of Toaster.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.mars3142.android.toaster.comparator;
 
 import org.mars3142.android.toaster.card.ToastCard;
